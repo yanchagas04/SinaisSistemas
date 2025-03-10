6521d6bb4e9b962d1a5feaada32b9519f6cb313b
Yan Chagas <yanchagas2004@gmail.com>
2025-03-10 08:33:53 -0300

    feat: Questão 3

---
+++ q3.py
@@ -0,0 +1,23 @@
+import functions.functions as functions
+import numpy as np
+import matplotlib.pyplot as plt
+
+MIN = -4
+MAX = 4
+T = 6
+
+degx = np.linspace(MIN, MAX, num=functions.calculate_points(MIN, MAX))
+
+def f1():
+    return (degx * (1/3)) + 1
+
+def f2():
+    return (degx * (-1 * 1/3)) + 1
+
+y1 = functions.degrau(-1 * T/2, MIN, MAX, f1) - functions.degrau(0, MIN, MAX, f1)
+y2 = functions.degrau(0, MIN, MAX, f2) - functions.degrau(T/2, MIN, MAX, f2)
+
+degy = y1 + y2
+
+plt.plot(degx, degy)
+plt.show()
